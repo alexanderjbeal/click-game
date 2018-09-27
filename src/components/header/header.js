0977417d85c10afb6e6bcf15b98c7ec7e77de3ef
Astronaut Group <19156046+alexanderjbeal@users.noreply.github.com>
2018-09-27 17:48:36 -0500

    Initial Commit

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => { return (
+    <header className="App-header">
+    <div className="header-logo"><img src="/img/nba-logo.png"></img></div>
+    <div></div>
+    <div></div>
+    
+    </header>
+)};
+
+export default Header;
